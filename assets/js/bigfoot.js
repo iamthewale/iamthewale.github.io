579c864b1693bd418b02fe56cf364f9ee6478a6d
iamthewale <filip.witek93@gmail.com>
2014-10-06 10:53:43 +0100

    Bigfoot modifications

--- assets/js/bigfoot.js
+++ assets/js/bigfoot.js
@@ -89,9 +89,9 @@
                                             "alt=\"See Footnote {{FOOTNOTENUM}}\" " +
                                             "rel=\"footnote\"" +
                                             "data-footnote-content=\"{{FOOTNOTECONTENT}}\">" +
-                                                "<span class=\"footnote-star\" data-footnote-number=\"{{FOOTNOTENUM}}\"></span>" +
-                                                // "<span class=\"footnote-circle\"></span>" +
-                                                // "<span class=\"footnote-circle\"></span>" +
+                                                "<span class=\"footnote-circle\" data-footnote-number=\"{{FOOTNOTENUM}}\"></span>" +
+                                                "<span class=\"footnote-circle\"></span>" +
+                                                "<span class=\"footnote-circle\"></span>" +
                                         "</a></div>"
             }, options);
 
